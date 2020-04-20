c3c621ea6f8488098f749e219d9d8bee3e1b8be5
sunliang <462028476@qq.com>
2020-04-20 22:02:20 +0800

    初始化springbootmvc

---
+++ src/test/java/com/example/mvc/SpringbootmvcApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.mvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootmvcApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
